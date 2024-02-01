29431464c0c25fd95373bffa183e2e83142d5025
Uzi <jacobjiwoo@naver.com>
2024-02-02 03:29:55 +0900

    change CORS origin

--- second/second/src/main/frontend/vite.config.js
+++ second/second/src/main/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://second-nord.store",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
